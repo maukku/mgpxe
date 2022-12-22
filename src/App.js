0906f25b1543f38dbb9395d213673ee801259271
maukku <maurorisso17@gmail.com>
2022-12-22 11:27:08 +0100

    Searchbar component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import styled from "styled-components";
 
+import SearchBar from "./components/SearchBar";
+
+const Container = styled.div`
+  width: 70%;
+
+  margin: auto;
+`;
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container>
+      <SearchBar />
+    </Container>
+  );
 }
 
 export default App;
